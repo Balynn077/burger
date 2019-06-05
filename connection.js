8521179cdc7e891c0876dafb613f97c4072d410c
Balynn077 <49453775+Balynn077@users.noreply.github.com>
2019-06-04 19:13:19 -0500

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+var connection;
+
+
+
+//JAWSDB for Heroku deployment
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
+
+connection.connect();
+
+module.exports = connection;
